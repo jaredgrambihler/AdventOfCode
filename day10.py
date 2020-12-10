1fa113c0906e6b4aeb9dac46c503173f8d56f30b
JaredGrambihler <jaredgrambihler@gmail.com>
2020-12-09 22:11:20 -0800

    Add part 2

--- day10.py
+++ day10.py
@@ -22,5 +22,22 @@ def solve(inputF):
         joltl.append(jolts)
     return ones * (threes+1)
 
-print(solve("input/day10test.txt"))
-print(solve("input/day10input.txt"))
+
+def solve2(inputF):
+    with open(inputF, 'r') as f:
+        text = f.read()
+    l = [int(x) for x in text.splitlines() if x.strip() != ""]
+    l.sort()
+    l = [0] + l
+    l = l + [l[-1] + 3]
+    combinations = {0:1}
+    for x in l[1:]:
+        prev1 = combinations.get(x-1, 0)
+        prev2 = combinations.get(x-2, 0)
+        prev3 = combinations.get(x-3, 0)
+        cur = prev1 + prev2 + prev3
+        combinations[x] = cur
+    return max(combinations.values())
+
+print(solve2("input/day10test.txt"))
+print(solve2("input/day10input.txt"))
